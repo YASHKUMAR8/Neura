88edbd8cba4133cdf81526321b1936be68e76bff
YASHKUMAR8 <125852197+YASHKUMAR8@users.noreply.github.com>
2025-02-23 18:17:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+        document.addEventListener("DOMContentLoaded", function () {
+            VANTA.NET({
+                el: "#vanta-bg",
+                mouseControls: true,
+                touchControls: true,
+                gyroControls: false,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                scale: 1.00,
+                scaleMobile: 1.00,
+                color: 0x46b88,
+                backgroundColor: "#000017", // Use hex format
+                points: 20.00,
+                maxDistance: 22.00,
+                spacing: 14.00
+            });
+        });
+
